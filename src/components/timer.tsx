ded403a8c990c76ac5b1c8e741698f498851fb76
pauchiner <calmgamessuport@gmail.com>
2022-09-29 17:50:48 +0200

    Added Auto Resizable Timer Text

--- src/components/timer.tsx
+++ src/components/timer.tsx
@@ -1,9 +1,18 @@
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import { Box, Text } from "@chakra-ui/react";
 import { Context } from "../store/context";
+import { motion } from 'framer-motion';
 
 const Timer = () => {
   const { timeLeft }: any = useContext(Context);
+  const [screenSize, getScreenSize] = useState({ width: window.innerWidth, height: window.innerHeight });
+
+  useEffect(() => {
+    window.addEventListener("resize", setScreenSize)
+    return (() => {
+      window.removeEventListener("resize", setScreenSize)
+    })
+  }, [screenSize])
 
   const getTime = () => {
     var minutes = Math.floor((timeLeft % 3600) / 60);
@@ -12,9 +21,17 @@ const Timer = () => {
     return ("0" + minutes).slice(-2) + ":" + ("0" + seconds).slice(-2);
   };
 
+  const getTextSize = () => {
+    return (screenSize.width / screenSize.height)
+  }
+
+  const setScreenSize = () => {
+    getScreenSize({ width: window.innerWidth, height: window.innerHeight });
+  }
+
   return (
     <Box textAlign="center">
-      <Text fontSize="4xl" fontWeight={500}>
+      <Text as={motion.h1} initial={{ zoom: 0.7 }} animate={{ zoom: getTextSize() }} fontSize="4xl" fontWeight={500}>
         {timeLeft >= 0 ? getTime() : "00:00"}
       </Text>
     </Box>
