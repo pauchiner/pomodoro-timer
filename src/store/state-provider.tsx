b2dfa3db234f55e31a82929b4d3fb4d2bdfa1e12
Pau Garcia Chiner <49660697+pauchiner@users.noreply.github.com>
2024-01-30 10:25:51 +0100

    feat: add a new electron boilerplate

--- src/store/state-provider.tsx
+++
@@ -1,8 +0,0 @@
-import { Context } from "./context";
-import { useGlobalState } from "./use-global-state";
-
-export const StateProvider = ({ children }: any) => {
-  const store = useGlobalState();
-
-  return <Context.Provider value={store}>{children}</Context.Provider>;
-};
